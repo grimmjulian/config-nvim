b7263c39475eaf9f10daf1eb84ff81ca8a69ab80
Julian Grimm <grimmjulian225@gmail.com>
2024-06-27 16:00:27 +0200

    Add keymap to search for diagnostics with telescope

--- lua/plugins/fuzzy_find.lua
+++ lua/plugins/fuzzy_find.lua
@@ -1,20 +1,20 @@
 local builtin = require("telescope.builtin")
 
-return{
+return {
   {
     "nvim-telescope/telescope.nvim",
     tag = '0.1.5',
-    opts = { extensions_list = { "fzf"  } },
+    opts = { extensions_list = { "fzf" } },
     dependencies = {
       { 'nvim-lua/plenary.nvim' },
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     keys = {
-      {"<leader>ff", builtin.find_files, desc = "Find in file names in dir"},
-      {'<leader>fg', builtin.live_grep, desc = "Find in files in dir"},
-      {'<leader>fb', builtin.buffers, desc = "Find in buffers"},
-      {'<leader>fh', builtin.help_tags, desc = "Find in help tags"},
+      { "<leader>ff", builtin.find_files,  desc = "Find in file names in dir" },
+      { '<leader>fg', builtin.live_grep,   desc = "Find in files in dir" },
+      { '<leader>fb', builtin.buffers,     desc = "Find in buffers" },
+      { '<leader>fh', builtin.help_tags,   desc = "Find in help tags" },
+      { '<leader>fd', builtin.diagnostics, desc = "Find in help tags" },
     }
   }
 }
-
