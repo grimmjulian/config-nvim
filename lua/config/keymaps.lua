cbc2aa01b8ad13493574bba4a2cc0e9afd5f32d6
Julian Grimm <grimmjulian225@gmail.com>
2024-06-28 00:03:38 +0200

    Add more keymaps based of primeagen

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>e', vim.cmd.Oil)
 vim.keymap.set('n', '<leader>so', ":w<CR>:source %<CR>")
+
+-- Move blocks of text up and down
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+-- fix cursor in half page scrolls
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- fix cursor in jumping through search results
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- if pasting on visual, text in register does not get overwritten
+vim.keymap.set("x", "<leader>p", "\"_dP")
