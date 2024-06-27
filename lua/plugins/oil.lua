7544ca04449163f84a2102cf8a51fa50ba73c123
Julian Grimm <grimmjulian225@gmail.com>
2024-06-27 16:08:15 +0200

    Set options for oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,14 @@
 return ({
   'stevearc/oil.nvim',
-  opts = {},
+  opts = {
+    view_options = {
+      show_hidden = true,
+      is_always_hidden = function(name, _)
+        return name == ".." or name == ".git"
+      end,
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true }
+  },
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
 })
