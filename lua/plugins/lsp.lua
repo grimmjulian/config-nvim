25ef2a1fd01299e83415ea85869603fcc6e1e84a
Julian Grimm <grimmjulian225@gmail.com>
2024-06-27 15:25:29 +0200

    Add lazydev for better lua language server

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,11 +1,9 @@
 return ({
   { 'williamboman/mason.nvim' },
   { 'williamboman/mason-lspconfig.nvim' },
-
   { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x',
     config = function()
       local lsp_zero = require('lsp-zero')
-
       lsp_zero.on_attach(function(client, bufnr)
         -- see :help lsp-zero-keybindings
         -- to learn the available actions
@@ -34,5 +32,26 @@ return ({
   { 'neovim/nvim-lspconfig' },
   { 'hrsh7th/cmp-nvim-lsp' },
   { 'hrsh7th/nvim-cmp' },
+  { "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+  {                                        -- optional completion source for require statements and module annotations
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, {
+        name = "lazydev",
+        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+      })
+    end,
+  },
   { 'L3MON4D3/LuaSnip' },
 })
