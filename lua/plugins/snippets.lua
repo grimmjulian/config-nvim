f4b3946494cc5f18222c0ff94084bade33530531
Julian Grimm <grimmjulian225@gmail.com>
2024-06-28 07:57:19 +0200

    Add friendly snippets

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -24,7 +24,11 @@ return ({
   version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
   -- install jsregexp (optional!).
   build = "make install_jsregexp",
-  dependencies = { "rafamadriz/friendly-snippets" },
+  dependencies = { "rafamadriz/friendly-snippets",
+  },
+  config = function()
+    require("luasnip.loaders.from_vscode").lazy_load()
+  end,
   keys = {
     { "<c-n>", jump_forward,  mode = { "i", "s" }, desc = "Snippet jump forward" },
     { "<c-p>", jump_backward, mode = { "i", "s" }, desc = "Snippet jump backward" },
