7323ed8cd54614ef263abd084078e9869671ed34
Julian Grimm <grimmjulian225@gmail.com>
2024-07-08 22:50:44 +0200

    Add code for testing and checking R code

--- ftplugin/r.lua
+++ ftplugin/r.lua
@@ -2,3 +2,6 @@ vim.opt.foldmethod = "indent"
 vim.opt.colorcolumn = "80"
 
 vim.g.r_indent_align_args = 0
+
+vim.keymap.set('n', '<leader>t', ":RSend devtools::test()<CR>")
+vim.keymap.set('n', '<leader>c', ":RSend devtools::check()<CR>")
