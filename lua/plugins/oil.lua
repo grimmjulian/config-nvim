ab0689251fc6c60556bf874e73b7107c8f90e1eb
Julian Grimm <grimmjulian225@gmail.com>
2024-07-10 12:05:06 +0200

    Add git signs to oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,4 +1,4 @@
-return ({
+return ({ {
   'stevearc/oil.nvim',
   opts = {
     view_options = {
@@ -7,8 +7,20 @@ return ({
         return name == ".." or name == ".git"
       end,
       delete_to_trash = true,
-      skip_confirm_for_simple_edits = true }
+      skip_confirm_for_simple_edits = true },
+    win_options = {
+      signcolumn = "yes:2",
+    },
   },
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
-})
+},
+  {
+    "refractalize/oil-git-status.nvim",
+
+    dependencies = {
+      "stevearc/oil.nvim",
+    },
+
+    config = true,
+  }, })
