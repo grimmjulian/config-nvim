f5a15755f1a52c74afc832a72452af1239f696a9
Julian Grimm <grimmjulian225@gmail.com>
2024-07-10 12:08:48 +0200

    Revert 1 commits
    ab06892 'Add git signs to oil'

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,4 +1,4 @@
-return ({ {
+return ({
   'stevearc/oil.nvim',
   opts = {
     view_options = {
@@ -7,20 +7,8 @@ return ({ {
         return name == ".." or name == ".git"
       end,
       delete_to_trash = true,
-      skip_confirm_for_simple_edits = true },
-    win_options = {
-      signcolumn = "yes:2",
-    },
+      skip_confirm_for_simple_edits = true }
   },
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
-},
-  {
-    "refractalize/oil-git-status.nvim",
-
-    dependencies = {
-      "stevearc/oil.nvim",
-    },
-
-    config = true,
-  }, })
+})
